127d6b73a7330fbde722ffbc01b213c333754348
Lucas Almeida <lucasdealmeida.ss@gmail.com>
2020-10-01 10:20:52 -0300

    Updates

---
+++ precache-manifest.592cf3a2735e40f364c7fb72bb879dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b7756502e32b5ed14127dd5daba62e",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "b14a8f2a90f20e8dcf97",
+    "url": "/react-quiz/static/js/2.1dc903d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.1dc903d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a2af1f82d8719d802f",
+    "url": "/react-quiz/static/js/main.29a82d8d.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  },
+  {
+    "revision": "4b11add2670c99faa82309c3a55dbf74",
+    "url": "/react-quiz/static/media/background.4b11add2.jpg"
+  }
+]);
